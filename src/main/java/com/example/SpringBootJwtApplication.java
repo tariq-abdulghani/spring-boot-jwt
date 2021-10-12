db846ea73ae43610dfb2bcafac4f5a38e589b76a
Tariq Abdulghnai <tariq.abdulghani.hussein@gmail.com>
2021-10-12 22:04:28 +0200

    added index and login and users end points.

--- src/main/java/com/example/SpringBootJwtApplication.java
+++ src/main/java/com/example/SpringBootJwtApplication.java
@@ -2,12 +2,31 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtApplication.class, args);
 	}
-
+	
+	
+	@GetMapping("/")
+	public String getIndex() {
+		return "Welcome !!";
+	}
+	
+	@PostMapping("/login")
+	public String login() {
+		return "";
+	}
+	
+	@GetMapping("/users")
+	public String getUsers() {
+		return "users Page";
+	}
 }
